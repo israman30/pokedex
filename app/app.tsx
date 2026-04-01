cfeda500c7ef1a409e4791499ce2c33f60ac6864
Israel Manzo <israelmanzo814@gmail.com>
2026-04-01 16:07:25 -0400

    hanlder game

--- app/app.tsx
+++ app/app.tsx
@@ -1,9 +1,12 @@
 import { StyleSheet } from "react-native";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 import Game from "./src/components/Game";
 
 export default function App() {
   return (
-    <Game />
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <Game />
+    </GestureHandlerRootView>
   );
 }
 
