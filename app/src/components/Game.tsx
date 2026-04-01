5fd1ed58bbfb96d6acb9f376b90ac458abc6038e
Israel Manzo <israelmanzo814@gmail.com>
2026-04-01 16:12:46 -0400

    added gesture event

--- app/src/components/Game.tsx
+++ app/src/components/Game.tsx
@@ -1,12 +1,23 @@
 import * as React from 'react';
 import { StyleSheet } from 'react-native';
+import { PanGestureHandler } from 'react-native-gesture-handler';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { Colors } from '../colors/color';
 
 export default function Game(): React.JSX.Element {
-	return <SafeAreaView style={styles.container}>
 
-    </SafeAreaView>;
+    function handleGesture(event: any) {
+        console.log("pressed", event.nativeEvent)
+    }
+
+    return (
+        <PanGestureHandler onGestureEvent={handleGesture}>
+            <SafeAreaView style={styles.container}>
+
+            </SafeAreaView>
+        </PanGestureHandler>
+
+    );
 }
 
 // Styling
