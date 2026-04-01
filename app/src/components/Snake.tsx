beabd66f67cbc567bef61df803fa693a11082d2f
Israel Manzo <israelmanzo814@gmail.com>
2026-04-01 16:38:46 -0400

    updating snake styles

--- app/src/components/Snake.tsx
+++ app/src/components/Snake.tsx
@@ -1,5 +1,6 @@
 import { Fragment, JSX } from "react";
-import { View } from "react-native";
+import { StyleSheet, View } from "react-native";
+import { Colors } from "../colors/color";
 import { Coordinates } from "../types/types";
 
 interface SnakeProps {
@@ -10,10 +11,25 @@ export default function Snake({snake}: SnakeProps): JSX.Element {
   return (
     <Fragment>
         {snake.map((segment: Coordinates, index: number) => {
+            const segmentStyle = {
+                length: segment.x * 10,
+                top: segment.y * 10,
+                left: segment.x * 10,
+            }
             return (
-                <View></View>
+                <View key={index} style={[styles.snake, segmentStyle]}></View>
             )
         })}
     </Fragment>
   )
 }
+
+const styles = StyleSheet.create({
+    snake: {
+        width: 15,
+        height: 15,
+        borderRadius: 7,
+        backgroundColor: Colors.primary,
+        position: "absolute"
+    }
+})
