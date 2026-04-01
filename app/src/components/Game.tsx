585a617d24217744d399699b749c895d9063883c
Israel Manzo <israelmanzo814@gmail.com>
2026-04-01 16:22:31 -0400

    added initial values const

--- app/src/components/Game.tsx
+++ app/src/components/Game.tsx
@@ -5,6 +5,12 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 import { Colors } from '../colors/color';
 import { GestureEventType } from '../types/types';
 
+const SNAKE_INITIAL_POSITION = [{ x: 5, y: 5 }];
+const FOOD_INITIAL_POSITION = { x: 10, y: 10 };
+const GAME_BOUNDS = { xMin: 0, xMax: 35, yMin: 0, yMax: 63 };
+const MOVE_INTERVAL = 200; // milliseconds
+const SCORE_INCREMENT = 10;
+
 export default function Game(): React.JSX.Element {
     // handling user drag finger on the screen
     function handleGesture(event: GestureEventType) {
--- app/src/types/types.tsx
+++ app/src/types/types.tsx
@@ -9,3 +9,10 @@ export interface Coordinates {
     x: number;
     y: number;
 }
+
+export enum Direction {
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT
+}
