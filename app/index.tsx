3ddee5a3f024810f294d90faff2c0a396349d265
Israel Manzo <israelmanzo814@gmail.com>
2026-03-31 23:02:56 -0400

    fetching pockemon api

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,26 @@
+import { useEffect, useState } from "react";
 import { Text, View } from "react-native";
 
 export default function Index() {
+
+  const [pokemons, setPokemons] = useState([]);
+
+  useEffect(() => {
+    // fetch pokemons
+    fetchPokemon();
+  }, [])
+
+  async function fetchPokemon() {
+    try {
+      const response = await fetch("https://pokeapi.co/api/v2/pokemon/?limit=20&offset=20");
+      const data = await response.json();
+      // console.log(data)
+      setPokemons(data);
+    } catch(e) {
+      console.log(e)
+    }
+  }
+
   return (
     <View
       style={{
@@ -9,7 +29,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>First changes to commit.</Text>
     </View>
   );
 }
